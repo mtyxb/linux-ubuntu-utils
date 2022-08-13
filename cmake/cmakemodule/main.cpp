4d7add488962afb8cb94355336654a1059e2e5e9
mtyxb <110961728+mtyxb@users.noreply.github.com>
2022-08-13 12:54:34 +0800

    cmake module definition for find_package

---
+++ cmake/cmakemodule/main.cpp
@@ -0,0 +1,13 @@
+#include "dll.h"
+#include <iostream>
+#include <limits>
+#include <cstddef>
+#include <iostream>
+
+int main()
+{
+    std::cout << __cplusplus << std::endl;
+    dll_func();   	
+    return 0;
+}
+
